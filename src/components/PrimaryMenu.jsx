82a879152ce534f606ee78dc5d9e4eedb34bb272
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-01-27 14:45:36 -0500

    feat: add primary menu

---
+++ src/components/PrimaryMenu.jsx
@@ -0,0 +1,49 @@
+import { Box, FormControl, MenuItem, Select } from '@mui/material';
+import { useState } from 'react';
+import CustomizedMenu from './CustomizedMenu';
+import { flightTypes, travelClass } from '../services/dataMenu';
+
+function PrimaryMenu() {
+  const [trip, setTrip] = useState('One-Way');
+  const [tripClass, setTripClass] = useState('Economy');
+
+  const handleChange = (event) => {
+    setTrip(event.target.value);
+  };
+  const handleChangeTripClass = (event) => {
+    setTripClass(event.target.value);
+  };
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        gap: 4,
+        flexDirection: { xs: 'column', md: 'row' },
+        flexWrap: 'wrap',
+        justifyContent: { xs: 'center', md: 'start' },
+      }}
+    >
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <Select value={trip} onChange={handleChange} displayEmpty>
+          {flightTypes.map((type) => (
+            <MenuItem value={type} key={type}>
+              {type}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <Select value={tripClass} onChange={handleChangeTripClass} displayEmpty>
+          {travelClass.map((singleTravelClass) => (
+            <MenuItem value={singleTravelClass} key={singleTravelClass}>
+              {singleTravelClass}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <CustomizedMenu />
+    </Box>
+  );
+}
+
+export default PrimaryMenu;
