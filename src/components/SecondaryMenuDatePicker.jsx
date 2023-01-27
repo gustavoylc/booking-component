e7b87bcbc7f963a8bb34938ff9ab7aadf597cc3a
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-01-27 14:46:11 -0500

    feat: add date picker component

---
+++ src/components/SecondaryMenuDatePicker.jsx
@@ -0,0 +1,33 @@
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { useState } from 'react';
+
+function SecondaryMenuDatePicker() {
+  const [value, setValue] = useState([null, null]);
+
+  return (
+    <LocalizationProvider
+      dateAdapter={AdapterDayjs}
+      localeText={{ start: 'Departure Date', end: 'Return Date' }}
+    >
+      <DateRangePicker
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(startProps, endProps) => (
+          <>
+            <TextField {...startProps} />
+            <Box sx={{ mx: 2 }}> to </Box>
+            <TextField {...endProps} />
+          </>
+        )}
+      />
+    </LocalizationProvider>
+  );
+}
+
+export default SecondaryMenuDatePicker;
