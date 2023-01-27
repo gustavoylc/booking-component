b65def25ba43e73bcdd44440e6299ed59fa7ec8d
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-01-27 14:55:53 -0500

    fix: delete unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { CssBaseline } from '@mui/material';
 import Home from './pages/Home';
-import { BookingProvider } from './context/BookingContext';
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <BookingProvider>
-        <Home />
-      </BookingProvider>
+      <Home />
     </>
   );
 }
